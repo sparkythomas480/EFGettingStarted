cc203a4af331f48012777454ea2c39e5c39d0f6f
Mark Thomas Watkins <mark.thomas480@gmail.com>
2024-09-21 18:41:40 +0200

    Initial commit

---
+++ Model.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    public BloggingContext()
+    {
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=NEWLAPPY;Database=Blogging;Trusted_Connection=True;Encrypt=false");
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    [NotMapped]
+    public DateTime LoadedFromDatabase { get; set; }
+
+    public DateTime CreatedTimestamp { get; set; }
+
+    public List<Post> Posts { get; } = new();
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
